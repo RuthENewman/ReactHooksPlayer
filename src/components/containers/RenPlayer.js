f03c50072d6449c60bf8e5f660ad533c9a345760
Ruth Newman <ruthnewman@gmail.com>
2020-10-15 23:57:48 +0100

    first commit

---
+++ src/components/containers/RenPlayer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Video from '../Video';
+import Playlist from '../containers/Playlist';
+
+const RenPlayer = props => {
+    return (
+        <>
+            <Video />
+            <Playlist />
+        </>
+    );
+}
+
+export default RenPlayer;
