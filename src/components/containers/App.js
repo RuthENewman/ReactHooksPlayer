b2ce264bdc35434d0d6c5bd1a2e79a94adc443dc
Ruth Newman <ruthnewman@gmail.com>
2020-10-16 18:32:00 +0100

    applying Global Style component

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import RenPlayer from './RenPlayer';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalStyle from '../styles/GlobalStyle';
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={RenPlayer} />
-      <Route exact path="/:activeVideo" component={RenPlayer} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={RenPlayer} />
+        <Route exact path="/:activeVideo" component={RenPlayer} />
+      </Switch>
+      <GlobalStyle />
+    </>
   </BrowserRouter>
 );
 
